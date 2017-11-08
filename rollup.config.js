d20b20d244f0552c1be99d946324f2d3befb0af9
Jorge Rodríguez Galán <jrogalan@outlook.com>
2017-11-08 16:55:59 +0100

    feat(angular5): update to angular5 version and build

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,37 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from "rollup-plugin-node-resolve";
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
-// Add here external dependencies that actually you use.
+/**
+  * Add here external dependencies that actually you use.
+  *
+  * About RxJS
+  * Each RxJS functionality that you use in the library must be added as external dependency.
+  * - For main classes use 'Rx':
+  *      e.g. import { Observable } from 'rxjs/Observable'; => 'rxjs/Observable': 'Rx'
+  * - For observable methods use 'Rx.Observable':
+  *      e.g. import 'rxjs/add/observable/merge'; => 'rxjs/add/observable/merge': 'Rx.Observable'
+  *      or for lettable operators:
+  *      e.g. import { merge } from 'rxjs/observable/merge'; => 'rxjs/observable/merge': 'Rx.Observable'
+  * - For operators use 'Rx.Observable.prototype':
+  *      e.g. import 'rxjs/add/operator/map'; => 'rxjs/add/operator/map': 'Rx.Observable.prototype'
+  *      or for lettable operators:
+  *      e.g. import { map } from 'rxjs/operators'; => 'rxjs/operators': 'Rx.Observable.prototype'
+  */
 const globals = {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    'rxjs/Observable': 'Rx',
-    'rxjs/Observer': 'Rx',
-    'rxjs/add/operator/map': 'Rx'
+  "@angular/core": "ng.core",
+  "@angular/common": "ng.common",
+  "rxjs/Observable": "Rx",
+  "rxjs/Observer": "Rx"
 };
 
 export default {
-    entry: './dist/modules/mallorca-ng.es5.js',
-    dest: './dist/bundles/mallorca-ng.umd.js',
-    format: 'umd',
-    exports: 'named',
-    moduleName: 'ng.mallorcaNG',
-    plugins: [resolve()],
-    external: Object.keys(globals),
+  plugins: [resolve(), sourcemaps()],
+  onwarn: () => { return; },
+  output: {
+    format: "umd",
+    name: "ng.angularLibraryStarter",
     globals: globals,
-    onwarn: () => { return }
-}
+    sourcemap: true,
+    exports: "named"
+  }
+};
